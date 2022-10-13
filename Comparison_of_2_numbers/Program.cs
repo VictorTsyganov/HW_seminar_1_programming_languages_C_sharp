1662c0440a6a1af7a246079e9de7a0fc9bedf9b3
Victor Tsyganov <tsyganov1989@gmail.com>
2022-10-13 16:38:00 +0300

    add Comparison_of_2_numbers

---
+++ Comparison_of_2_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int numberone = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int numbertwo = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if(numberone >= numbertwo)
+{
+    max = numberone;
+}
+else
+{
+    max = numbertwo;
+}
+
+Console.Write($"max = {max}");
