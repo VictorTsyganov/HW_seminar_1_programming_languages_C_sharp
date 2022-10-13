449c32ac7e1f882addf0120a7b157f7c81d4e9d8
Victor Tsyganov <tsyganov1989@gmail.com>
2022-10-13 17:21:17 +0300

    add Even_or_not_even_number

---
+++ Even_or_not_even_number/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numberone = int.Parse(Console.ReadLine());
+
+if(numberone % 2 == 0)
+{
+    Console.Write($"Да. Число {numberone} четное.");
+}
+else
+{
+    Console.Write($"Нет. Число {numberone} нечетное.");
+}
