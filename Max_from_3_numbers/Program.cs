1ed741364b8dfe7b44830169f8d774d541ac8dbf
Victor Tsyganov <tsyganov1989@gmail.com>
2022-10-13 17:06:45 +0300

    add Max_from_3_numbers

---
+++ Max_from_3_numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int numberone = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int numbertwo = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int numberthree = int.Parse(Console.ReadLine());
+
+int max = numberone;
+
+if(max <= numbertwo)
+{
+    max = numbertwo;
+}
+if(max <= numberthree)
+{
+    max = numberthree;
+}
+Console.Write($"-> {max}");
